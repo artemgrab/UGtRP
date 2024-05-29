ec3f6057de09ad3bda303df97f7b1fff739cb289
Artem Hrabovskyi <artemygrab@gmail.com>
2024-05-29 16:02:31 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-let guns = document.querySelectorAll('.desc_block_guns')
-let furniture = document.querySelectorAll('.desc_block_furniture')
-
+let guns = document.querySelector('.desc_block_guns')
+let furniture = document.querySelector('.desc_block_furniture')
+let partners = document.querySelector('.layout_grid')
+let button = document.querySelector('#button-partners')
 
 function f1() {
     console.log(1)
@@ -19,13 +20,30 @@ function f2() {
 
 function f3() {
 
+    console.log(1)
+    guns.classList.remove('hide');
+    furniture.classList.add('hide');
+
+    
+}
+function f4() {
+    console.log(2)
+    guns.classList.add('hide');
+    furniture.classList.remove('hide');
+}
+function f5() {
+    partners.classList.toggle('none')
+
 }
 
 
 
 
 
-document.querySelectorAll('#b-1').onclick = f1;
-document.querySelectorAll('#b-2').onclick = f2;
+document.querySelector('#b-1').onclick = f1;
+document.querySelector('#b-2').onclick = f2;
+document.querySelector('#b2-1').onclick = f3;
+document.querySelector('#b2-2').onclick = f4;
+document.querySelector('#button-partners').onclick = f5;
 
 
