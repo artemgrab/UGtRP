7a3d7cf6a27c41ffdf00d6f4e7c20b0e8cd7e063
Artem Hrabovskyi <artemygrab@gmail.com>
2024-05-29 14:36:24 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-let guns = document.querySelector('.desc_block_guns')
-let cloth = document.querySelector('.desc_block_cloth')
+let guns = document.querySelectorAll('.desc_block_guns')
+let furniture = document.querySelectorAll('.desc_block_furniture')
 
 
 function f1() {
     console.log(1)
     guns.classList.remove('hide');
-    cloth.classList.add('hide');
+    furniture.classList.add('hide');
+
 }
 
 
 function f2() {
     console.log(2)
     guns.classList.add('hide');
-    cloth.classList.remove('hide');
+    furniture.classList.remove('hide');
 }
 
 
@@ -24,23 +25,7 @@ function f3() {
 
 
 
-document.querySelector('#b-1').onclick = f1;
-document.querySelector('#b-2').onclick = f2;
+document.querySelectorAll('#b-1').onclick = f1;
+document.querySelectorAll('#b-2').onclick = f2;
 
 
-// window.onscroll = function() {myFunction()};
-
-// // Get the header
-// let header = document.getElementById("header");
-
-// // Get the offset position of the navbar
-// let sticky = header.offsetTop;
-
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
