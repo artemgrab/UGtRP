ea3e0b15756c4dcfcd5368e08be4e92b9e32c634
Artem Hrabovskyi <artemygrab@gmail.com>
2024-06-19 15:25:49 +0300

    up

--- js/script.js
+++ js/script.js
@@ -171,17 +171,13 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-  
-const hasVisited = localStorage.getItem('hasVisited');
 
-if(!hasVisited) {
-  
-    localStorage.setItem('hasVisited', true)
-  
-    // Do something
-  
-} else {
-  
-    // Do something else
-  
-}
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
